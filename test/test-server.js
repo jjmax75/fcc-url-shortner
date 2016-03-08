56d9d61e6e69d54077dde8472ea7694ab6a3101e
John Behan <behanj@gmail.com>
2016-03-08 10:57:54 +0700

    test http for current routes

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-// var server = require('../server.js');
+var server = require('../server.js');
 var should = chai.should();
 
 chai.use(chaiHttp);
@@ -11,5 +11,51 @@ describe('Simple Tests', function() {
   });
   it('A simple number test', function() {
     Number('1').should.be.a('number');
+  });
+});
+
+describe('App specific tests', function() {
+  describe('Page responses, type & content', function() {
+    it('Should display the homepage, /, GET', function(done) {
+      chai.request(server)
+        .get('/')
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.html;
+          res.should.be.a('object');
+          done();
+        });
+    });
+    it('Should display the urls page, /api/urls, GET', function(done) {
+      chai.request(server)
+        .get('/api/urls')
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.should.be.a('object');
+          done();
+        });
+    });
+    it('Should display the new page, /api/new/:url, GET', function(done) {
+      chai.request(server)
+        .get('/api/new/boxitoff.com')
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.should.be.a('object');
+          done();
+        });
+    });
+  });
+
+  describe('Adding a URL to DB, /api/new/:url', function() {
+    it('Should extract the url from the parameter', function(done) {
+      chai.request(server)
+        .get('/api/new/boxitoff.com')
+        .end(function(err, res) {
+
+          done();
+        });
+    });
   });
 });
